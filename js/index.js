9d28bb518548026495b4a018f4d695834830dcfa
Muhammad Agung Hikmatullah <48979952+MAgungHKM@users.noreply.github.com>
2023-08-29 19:03:49 +0700

    Fix crash on Android
    Maximum call stack size exceeded on Android platform

--- js/index.js
+++ js/index.js
@@ -6,62 +6,62 @@ import Ajv from 'ajv';
 
 const ajv = new Ajv({});
 
-const iosValidate = ajv.compile({
-  title: 'TrustKitConfiguration',
-  type: 'object',
-  properties: {
-    SwizzleNetworkDelegates: {
-      type: 'boolean',
-    },
-    PinnedDomain: {
-      type: 'object',
-      patternProperties: {
-        '^.*$': {
-          type: 'object',
-          properties: {
-            PublicKeyAlgorithms: {
-              type: 'array',
-              minItems: 1,
-              items: {
-                enum: ['AlgorithmRsa2048', 'AlgorithmRsa4096'],
-              },
-            },
-            PublicKeyHashes: {
-              type: 'array',
-              minItems: 2,
-              items: {
-                oneOf: [
-                  { type: 'string' },
-                ],
-              },
-            },
-            IncludeSubdomains: {
-              type: 'boolean',
-            },
-            EnforcePinning: {
-              type: 'boolean',
-            },
-            ReportUris: {
-              type: 'array',
-              items: {
-                oneOf: [
-                  { type: 'string' },
-                ],
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-});
-
 export default (configuration) => {
   const { RNTrustKitPlugin } = NativeModules;
   return new Promise((resolve, reject) => {
     if (Platform.OS === 'android') {
       reject(new Error('Android not currently supported'));
     } else if (Platform.OS === 'ios') {
+      const iosValidate = ajv.compile({
+        title: 'TrustKitConfiguration',
+        type: 'object',
+        properties: {
+          SwizzleNetworkDelegates: {
+            type: 'boolean',
+          },
+          PinnedDomain: {
+            type: 'object',
+            patternProperties: {
+              '^.*$': {
+                type: 'object',
+                properties: {
+                  PublicKeyAlgorithms: {
+                    type: 'array',
+                    minItems: 1,
+                    items: {
+                      enum: ['AlgorithmRsa2048', 'AlgorithmRsa4096'],
+                    },
+                  },
+                  PublicKeyHashes: {
+                    type: 'array',
+                    minItems: 2,
+                    items: {
+                      oneOf: [
+                        { type: 'string' },
+                      ],
+                    },
+                  },
+                  IncludeSubdomains: {
+                    type: 'boolean',
+                  },
+                  EnforcePinning: {
+                    type: 'boolean',
+                  },
+                  ReportUris: {
+                    type: 'array',
+                    items: {
+                      oneOf: [
+                        { type: 'string' },
+                      ],
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+
       if (configuration && iosValidate(configuration)) {
         resolve(RNTrustKitPlugin.configure(configuration));
       } else if (!configuration) {
